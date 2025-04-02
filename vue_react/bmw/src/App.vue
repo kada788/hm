f9b920122378eb204128d45f5af0b2c396654515
kada778 <2388680784@qq.com>
2025-04-02 22:17:29 +0800

    code

---
+++ vue_react/bmw/src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import LifecycleComponent from './components/LifecycleComponent.vue'
+import { ref } from 'vue'
+// option 选项式API -> 组合式 Composition API
+const showComponent = ref(true)
+const  toggltComponent = () => {
+ showComponent.value = !showComponent.value
+}
+
+const count = ref(0)
+const width = ref(100)
+const  incrementCount = () => {
+  count.value++
+  width.value+=99
+}
+</script>
+
+<template>
+  <div>
+   <button @click="toggltComponent"> Toggle Component</button>
+   <button @click="incrementCount">incrementCount</button>
+   <p>Count: {{count}}</p>
+   <!-- vue 的内置组件 -->
+   <transition name="fade">
+   <LifecycleComponent v-show="showComponent" :count="count" />
+   </transition>
+    <div class="box1" :style="{width:width+'px'}" 
+    style="background-color:aqua;height: 100px;transition: width 1s;"></div>
+    <button @click="incrementCount">click</button>
+  </div>
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ vue_react/bmw/src/components/LifecycleComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div style="background-color: aqua;">
+      <p>Lifecycle Component - Count: {{count}}</p>
+    </div>
+</template>
+
+<script setup>
+import { defineProps,
+    onMounted,
+    onBeforeUnmount,
+    onUpdated,
+    onBeforeUpdate
+} from 'vue';
+defineProps ({
+    count: {
+        type: Number,
+        required: true // 必须传入
+           }
+})
+// 组件由生老病死
+onMounted(() => {
+    // 已经完成了挂载
+    console.log('组件挂载中');
+})
+onBeforeUnmount(() => {
+    console.log('组件卸载前'); 
+})
+onUpdated(() => {
+    console.log('组件更新后'); 
+})
+onBeforeUpdate(() => {
+    console.log('组件更新前'); 
+})
+</script>
+
+<style  scoped>
+
+</style>
